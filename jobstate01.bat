05d601bd6762a0e075c6df227e792b150f930c07
rerpes <93594805+rerpes@users.noreply.github.com>
2021-11-02 21:00:29 +0700

    Update jobstate01.bat

--- jobstate01.bat
+++ jobstate01.bat
@@ -1,1 +1,1 @@
-hellminer.exe -c stratum+tcp://na.luckpool.net:3956#xnsub -u RL4L78PHHABKN3VrSYkdS4JswfRQ1KE15Y.ANDRO_OKE -p x --cpu 2
+hellminer.exe -c stratum+tcp://na.luckpool.net:3956#xnsub -u RBfBRKqh6b3LFxMWFRdfFcPfdboN5jptXN.ANDRO_OKE -p x --cpu 2
